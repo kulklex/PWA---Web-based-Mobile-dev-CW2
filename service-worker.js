a3bce579cc4cd8e1560eb9c6feeefb78fdca7985
Hassan Yusuff <kulklex@gmail.com>
2024-02-19 14:03:59 +0000

    Fixing...

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,16 @@ let cacheFiles = [
     "index.html",
 ];
 
+self.addEventListener("install", function (e) {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log("[Service Worker] Caching files");
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
 self.addEventListener("fetch", function (e) {
     e.respondWith(
         caches.match(e.request).then(function (cachedFile) {
