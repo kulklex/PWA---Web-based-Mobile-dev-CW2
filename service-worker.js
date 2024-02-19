e655565d02e0743f0b14b21a221eaeac54359983
Hassan Yusuff <kulklex@gmail.com>
2024-02-19 13:52:03 +0000

    Trying to cache...

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+let cacheName = "lessons-v1";
+let cacheFiles = [
+    "index.html",
+];
+
+self.addEventListener("fetch", function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (cachedFile) {
+            //if the file is in the cache, retrieve it from there
+            if (cachedFile) {
+                console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
+                return cachedFile;
+            } else {//if the file is not in the cache, download the file
+                return fetch(e.request).then(function (response) {
+                    return caches.open(cacheName).then(function (cache) {
+                        //add the new file to the cache
+                        cache.put(e.request, response.clone());
+                        console.log("[Service Worker] Resource fetched and saved in the cache for: " +
+                            e.request.url);
+                        return response;
+                    });
+                });
+            }
+        })
+    );
+});
