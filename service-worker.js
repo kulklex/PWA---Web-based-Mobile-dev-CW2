fd352b709395948ac80bfa8504f6f6885be13f24
Hassan Yusuff <kulklex@gmail.com>
2024-02-26 23:10:44 +0000

    sw fixes

--- service-worker.js
+++ service-worker.js
@@ -16,19 +16,29 @@ self.addEventListener("install", function (e) {
 self.addEventListener("fetch", function (e) {
     e.respondWith(
         caches.match(e.request).then(function (cachedFile) {
-            //if the file is in the cache, retrieve it from there
+            // If the file is in the cache, retrieve it from there
             if (cachedFile) {
                 console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
                 return cachedFile;
-            } else {//if the file is not in the cache, download the file
+            } else {
+                // If the file is not in the cache, download the file
                 return fetch(e.request).then(function (response) {
-                    return caches.open(cacheName).then(function (cache) {
-                        //add the new file to the cache
-                        cache.put(e.request, response.clone());
-                        console.log("[Service Worker] Resource fetched and saved in the cache for: " +
-                            e.request.url);
+                    // Check if the response can be cloned
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
                         return response;
+                    }
+
+                    // Clone the response and cache it
+                    var responseToCache = response.clone();
+                    caches.open(cacheName).then(function (cache) {
+                        cache.put(e.request, responseToCache);
                     });
+
+                    console.log("[Service Worker] Resource fetched and saved in the cache for: " +
+                        e.request.url);
+                    return response;
+                }).catch(function(error) {
+                    console.error("[Service Worker] Fetch error:", error);
                 });
             }
         })
