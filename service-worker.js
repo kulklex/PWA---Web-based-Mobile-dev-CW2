ccf000a953779d325a756c470c7dbafe076f37d7
Hassan Yusuff <kulklex@gmail.com>
2024-02-27 15:21:20 +0000

    More fixes

--- service-worker.js
+++ service-worker.js
@@ -13,24 +13,24 @@ self.addEventListener("install", function (e) {
     );
 });
 
-self.addEventListener("fetch", function(e) {
+self.addEventListener("fetch", function (e) {
     e.respondWith(
-    caches.match(e.request).then(function (cachedFile) {
-    //if the file is in the cache, retrieve it from there
-    if (cachedFile) {
-    console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
-    return cachedFile;
-    } else {//if the file is not in the cache, download the file
-    return fetch(e.request).then(function (response) {
-    return caches.open(cacheName).then(function (cache) {
-    //add the new file to the cache
-   cache.put(e.request, response.clone());
-    console.log("[Service Worker] Resource fetched and saved in the cache for: " + 
-   e.request.url);
-    return response;
-    });
-    });
-    }
-    })
-    ); 
-   });
+        caches.match(e.request).then(function (cachedFile) {
+            //if the file is in the cache, retrieve it from there
+            if (cachedFile) {
+                console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
+                return cachedFile;
+            } else {//if the file is not in the cache, download the file
+                return fetch(e.request).then(function (response) {
+                    return caches.open(cacheName).then(function (cache) {
+                        //add the new file to the cache
+                        cache.put(e.request, response.clone());
+                        console.log("[Service Worker] Resource fetched and saved in the cache for: " +
+                            e.request.url);
+                        return response;
+                    });
+                });
+            }
+        })
+    );
+});
